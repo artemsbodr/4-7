d362e30c0058b6cc2c68f3feba4ffde574e82862
MrArtem <artembodrik210311@gmail.com>
2025-08-30 01:13:14 +0300

    push

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { products } from '../data.js';
-import productTemplate from './product-list.hbs';
+import { products } from './data.js';
+import productTemplate from './temp/product.hbs';
 
 const html = productTemplate({ products });
 document.getElementById('app').innerHTML = html;

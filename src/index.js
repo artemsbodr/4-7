1d4767d3caf2591da675fc0d0c1ab3ff19ae0b63
MrArtem <artembodrik210311@gmail.com>
2025-08-30 01:04:56 +0300

    push2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import  Counter  from './counter.js';
-import { greet } from './un.js';
-
-
-const app = document.getElementById('app');
-app.textContent = greet('Parcel');
-
-const counter = new Counter(0, app);
-counter.init();
+п
